8e3e9f14efbb388a4cbc689c49197af4dfb433d1
DoosanD <davicdusan@gmail.com>
2022-06-20 14:51:44 +0200

    first commit

---
+++ front-page.php
@@ -0,0 +1,63 @@
+<?php get_header();?>
+
+  <body>
+    <div class="container p-5">
+      <hr class="divider" />
+      <p class="text-center">HOW IT WORKS</p>
+      <h2 class="text-center p-1">A Few Simple Steps to Your Monumental Move</h2>
+      <br />
+      <div class="row p3">
+        <div class="col p-3">  <div class="card shadow-lg" style="width: 21rem">
+       
+          <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/step-1.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/1.png" class="card-img-number" alt="number one">
+            <h4 class="card-title text-center ">Contact Us</h4>
+            <p class="card-text p-4 text-center">
+              Get in touch with Ben Hur  
+              Moving & Storage via phone or by filling out our
+              free online quote form.
+
+              
+            </p>
+          </div>
+        </div></div>
+        <div class="col p-3">
+          <div class="card shadow-lg" style="width: 21rem">
+            <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/step-2.jpg" alt="Card image cap" />
+            <div class="card-body">
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/2.png" class="card-img-number" alt="number two">
+              <h4 class="card-title text-center">Share Your Plans with Us</h4>
+              <p class="card-text p-4 text-center">
+               Let us know where and when you are planning to move and our moving and
+               storageNYC experts will come up with a customized
+              </p>      
+            </div>
+        </div></div>
+        <div class="col p-3"><div class="card shadow-lg" style="width: 21rem">
+          <img class="card-img-top" src="<?php echo get_stylesheet_directory_uri(); ?>/step-3.jpg" alt="Card image cap" />
+          <div class="card-body">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/3.png" class="card-img-number" alt="number three">
+            <h4 class="card-title text-center">Enjoy Your Epic Move</h4>
+            <p class="card-text p-4 text-center">
+              Not every relocation needs to be a stress-inducing and money-draining process - 
+              stay relaxed all through your moving process  
+            </p>  
+         </div>
+      </div>
+  </div>
+      </div>
+      
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
+
+
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+       <!-- Bootstrap CSS -->
+       <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" type="text/css" media="screen" />
+    <!-- <link rel="stylesheet" href="style.css" /> -->
+    <title>How It Works</title>
+ 
+  </head>
+<body <?php body_class();?>>
+    
